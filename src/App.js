6237381ab21a753fa541b3812fb96dc7bfb83f77
yomites <marshall_400@yahoo.com>
2020-07-16 12:59:09 +0300

    Exercise 5.5
    Exercise 5.5: Blog list frontend step 5: The form for creating blog posts is modified so that it is only displayed when needed.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import './App.css'
 import Blog from './components/Blog'
 import BlogForm from './forms/BlogForm'
 import blogService from './services/blogs'
 import Notification from './components/Notification'
+import Togglable from './forms/Togglable'
 import loginService from './services/login'
 import LoginForm from './forms/LoginForm'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
-  const [newTitle, setNewTitle] = useState('')
-  const [newAuthor, setNewAuthor] = useState('')
-  const [newUrl, setNewUrl] = useState('')
   const [notification, setNotification] = useState(null)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const blogFormRef = useRef()
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -32,7 +31,7 @@ const App = () => {
     }
   }, [])
 
-  const notifyWith = (message, type='success') => {
+  const notifyWith = (message, type = 'success') => {
     setNotification({ message, type })
     setTimeout(() => {
       setNotification(null)
@@ -59,18 +58,6 @@ const App = () => {
     }
   }
 
-  const handleTitleChange = (event) => {
-    setNewTitle(event.target.value)
-  }
-
-  const handleAuthorChange = (event) => {
-    setNewAuthor(event.target.value)
-  }
-
-  const handleUrlChange = (event) => {
-    setNewUrl(event.target.value)
-  }
-
   const handleUsernameChange = (event) => {
     setUsername(event.target.value)
   }
@@ -79,25 +66,16 @@ const App = () => {
     setPassword(event.target.value)
   }
 
-  const addBlog = (event) => {
-    event.preventDefault()
-    console.log('button clicked', event.target)
-    const blogObject = {
-      title: newTitle,
-      author: newAuthor,
-      url: newUrl,
-    }
-
+  const addBlog = (blogObject) => {
+    blogFormRef.current.toggleVisibility()
     blogService
       .create(blogObject)
       .then(returnedBlog => {
         setBlogs(blogs.concat(returnedBlog))
         notifyWith(`${returnedBlog.title} by ${returnedBlog.author} successfully added`)
-    
-        setNewTitle('')
-        setNewAuthor('')
-        setNewUrl('')
-      })
+      }).catch(error => {
+        notifyWith(`The fields can not be empty`, 'error')
+      })      
   }
 
   const logoutUser = () => {
@@ -105,6 +83,14 @@ const App = () => {
     setUser(null)
     window.localStorage.clear()
   }
+
+  const blogForm = () => (
+    <Togglable buttonLabel='new blog' ref={blogFormRef}>
+      <BlogForm
+        createBlog={addBlog}
+      />
+    </Togglable>
+  )
 
   if (user === null) {
     return (
@@ -129,15 +115,7 @@ const App = () => {
         {user.name} logged in
         <button onClick={logoutUser}>logout</button>
       </p>
-      <BlogForm
-        handleTitleChange={handleTitleChange}
-        handleAuthorChange={handleAuthorChange}
-        handleUrlChange={handleUrlChange}
-        addBlog={addBlog}
-        newTitle={newTitle}
-        newAuthor={newAuthor}
-        newUrl={newUrl}
-      />
+      {blogForm()}
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
--- src/forms/BlogForm.js
+++ src/forms/BlogForm.js
@@ -1,29 +1,57 @@
-import React from 'react'
-const BlogForm = (props) => {
+import React, { useState } from 'react'
+const BlogForm = ({ createBlog }) => {
+    const [newTitle, setNewTitle] = useState('')
+    const [newAuthor, setNewAuthor] = useState('')
+    const [newUrl, setNewUrl] = useState('')
+
+    const handleTitleChange = (event) => {
+        setNewTitle(event.target.value)
+    }
+
+    const handleAuthorChange = (event) => {
+        setNewAuthor(event.target.value)
+    }
+
+    const handleUrlChange = (event) => {
+        setNewUrl(event.target.value)
+    }
+
+    const addBlog = (event) => {
+        event.preventDefault()
+        createBlog({
+            title: newTitle,
+            author: newAuthor,
+            url: newUrl,
+        })
+
+        setNewTitle('')
+        setNewAuthor('')
+        setNewUrl('')
+    }
 
     return (
-        <form onSubmit={props.addBlog}>
+        <form onSubmit={addBlog}>
             <div>
                 <h2>create new</h2>
                 <div>
                     Title: <input
                         type="text"
-                        value={props.newTitle}
-                        onChange={props.handleTitleChange}
+                        value={newTitle}
+                        onChange={handleTitleChange}
                     />
                 </div>
                 <div>
                     Author: <input
                         type="text"
-                        value={props.newAuthor}
-                        onChange={props.handleAuthorChange}
+                        value={newAuthor}
+                        onChange={handleAuthorChange}
                     />
                 </div>
                 <div>
                     url: <input
                         type="text"
-                        value={props.newUrl}
-                        onChange={props.handleUrlChange}
+                        value={newUrl}
+                        onChange={handleUrlChange}
                     />
                 </div>
                 <div>
